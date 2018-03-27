f2ae0245fe1c66b0d872227221c28e274d4e1563
GW-GhostWolf <michael.poc@att.net>
2018-03-27 15:20:31 +0000

    Extend comments based on review feedback

--- movies.py
+++ movies.py
@@ -2,13 +2,29 @@ import webbrowser
 
 
 class Movie():
-    """Hold the movie title, storyline, poster image URL, trailer URL"""
+    """
+    Container for basic movie information
+        Title
+        Storyline
+        Poster Image
+        Trailer Link
+    """
     def __init__(self, title, story, image, trailer):
+        """
+        Constructor for Movie class
+            Input: title - Movie Title
+            Input: story - Storyline for the movie
+            Input: image - URL for a poster image
+            Input: trailer - URL for the movie trailer on youtube.com
+            Output: Instantiated movie object
+        """
         self.title = title
         self.storyline = story
         self.poster_image_url = image
         self.trailer_youtube_url = trailer
 
     def show_trailer(self):
-        """Show the movie's trailer"""
+        """
+        Show the movie's trailer
+        """
         webbrowser.open(self.youtube_trailer)
